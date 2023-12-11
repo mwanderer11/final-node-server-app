e991d544710ad0053c73a49c7815d14ed71c4ec4
maliawanderer <malia.wanderer@gmail.com>
2023-12-10 21:06:35 -0500

    mongoose

--- final-node-server-app/app.js
+++ final-node-server-app/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import "dotenv/config";
 import cors from "cors";
-//import mongoose from "mongoose";
+import mongoose from "mongoose";
 import PlayerRoutes from "./Players/routes"
 import UserRoutes from "./Users/routes"
-//mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
-const app = express();
+
+const CONNECTION_STRING = "mongodb"
+mongoose.connect(CONNECTION_STRING);const app = express();
+const db = mongoose.connection;
 app.use(cors());
 app.use(express.json());
 PlayerRoutes(app);
